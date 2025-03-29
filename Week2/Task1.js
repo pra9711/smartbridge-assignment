55afd999bb34600ade731ee77ff9f91b866fd8d6
Prabhat Raj <55561343+pra9711@users.noreply.github.com>
2025-03-30 00:41:42 +0530

    Create Task1.js

---
+++ Week2/Task1.js
@@ -0,0 +1,7 @@
+// Task 1: Declare a variable age and assign it a value of 25. Then, 
+// declare another variable isAdult that is true if age is 18 or more, otherwise false.
+let age = 25;
+let isAdult = age >= 18; 
+
+console.log("Task 1 - Age:", age);
+console.log("Task 1 - Is Adult:", isAdult);
