5a7d9c8ec27f1c6cc83b095cb9355b77e625da77
Prabhat Raj <55561343+pra9711@users.noreply.github.com>
2025-04-09 00:43:30 +0530

    Delete Week2 directory

--- Week2/task5.js
+++
@@ -1,8 +0,0 @@
-// Task 5: Write a function square that takes a number as input and 
-// returns its square.
-function square(num) {
-    return num * num;
-}
-
-let squaredValue = square(4); 
-console.log("Task 5 - Square of 4:", squaredValue);
